5fc40f3a020d745335e1f5f4bf14197450b3a3da
1F10220122 <s1F102201223@iniad.org>
2023-07-26 19:59:34 +0900

    修正

--- config/production.py
+++ config/production.py
@@ -14,4 +14,3 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
